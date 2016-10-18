a1e6d02a4b7bb2ecccab280badb5b5b202cec511
Ondřej Zálešák <ondrej.zalesak@wincor-nixdorf.cz>
2016-10-18 07:56:30 +0200

    První pokus.

---
+++ MonitoringAgent/MonitoringAgent/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Diagnostics;
+
+namespace MonitoringAgent
+{
+    class Program
+    {
+
+        private CompositionContainer _container;
+
+        private Program()
+        {
+            //An aggregate catalog that combines multiple catalogs
+            var catalog = new AggregateCatalog();
+            //Adds all the parts found in the same assembly as the Program class
+            catalog.Catalogs.Add(new AssemblyCatalog(typeof(Program).Assembly));
+            catalog.Catalogs.Add(new DirectoryCatalog(System.IO.Path.GetFullPath(@"..\..\Plugins\")));
+
+            //Create the CompositionContainer with the parts in the catalog
+            _container = new CompositionContainer(catalog);
+
+            //Fill the imports of this object
+            try
+            {
+                this._container.ComposeParts(this);
+            }
+            catch (CompositionException compositionException)
+            {
+                Console.WriteLine(compositionException.ToString());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine("Application is RUNNING");
+            Console.ReadLine();
+        }
+
+    }
+}
