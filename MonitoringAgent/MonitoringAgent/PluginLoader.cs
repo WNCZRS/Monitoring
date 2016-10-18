a686542ebe010b76ed5114751876fd447e86d62b
Ondřej Zálešák <ondrej.zalesak@wincor-nixdorf.cz>
2016-10-18 11:59:22 +0200

    PluginLoader created.

---
+++ MonitoringAgent/MonitoringAgent/PluginLoader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.IO;
+
+namespace MonitoringAgent
+{
+    public class PluginLoader
+    {
+        private CompositionContainer _container;
+
+        public void Loader()
+        {
+            if (!Directory.Exists("Plugins"))
+            {
+                Directory.CreateDirectory("Plugins");
+            }
+
+            //An aggregate catalog that combines multiple catalogs
+            var catalog = new AggregateCatalog();
+            //Adds all the parts found in the same assembly as the Program class
+            catalog.Catalogs.Add(new AssemblyCatalog(typeof(Program).Assembly));
+            catalog.Catalogs.Add(new DirectoryCatalog(System.IO.Path.GetFullPath(@"Plugins\")));
+
+            //Create the CompositionContainer with the parts in the catalog
+            _container = new CompositionContainer(catalog);
+
+            //Fill the imports of this object
+            try
+            {
+                this._container.ComposeParts(this);
+                foreach (var item in catalog)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+            catch (CompositionException compositionException)
+            {
+                Console.WriteLine(compositionException.ToString());
+            }
+        }
+
+    }
+}
--- MonitoringAgent/MonitoringAgent/Program.cs
+++ MonitoringAgent/MonitoringAgent/Program.cs
@@ -3,42 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.ComponentModel.Composition;
-using System.ComponentModel.Composition.Hosting;
 using System.Diagnostics;
+using System.IO;
 
 namespace MonitoringAgent
 {
-    class Program
+    static class Program
     {
-
-        private CompositionContainer _container;
-
-        private Program()
+        public static void Main(string[] args)
         {
-            //An aggregate catalog that combines multiple catalogs
-            var catalog = new AggregateCatalog();
-            //Adds all the parts found in the same assembly as the Program class
-            catalog.Catalogs.Add(new AssemblyCatalog(typeof(Program).Assembly));
-            catalog.Catalogs.Add(new DirectoryCatalog(System.IO.Path.GetFullPath(@"..\..\Plugins\")));
+            var instance = new PluginLoader();
+            instance.Loader();
 
-            //Create the CompositionContainer with the parts in the catalog
-            _container = new CompositionContainer(catalog);
-
-            //Fill the imports of this object
-            try
-            {
-                this._container.ComposeParts(this);
-            }
-            catch (CompositionException compositionException)
-            {
-                Console.WriteLine(compositionException.ToString());
-            }
-        }
-
-        static void Main(string[] args)
-        {
-            Program p = new Program();
             Console.WriteLine("Application is RUNNING");
             Console.ReadLine();
         }
