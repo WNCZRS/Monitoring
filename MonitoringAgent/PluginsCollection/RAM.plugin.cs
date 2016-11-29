8af8b5e9272846fcf78d709adabcc1c6192ef0d5
Ondřej Zálešák <ondrej.zalesak@wincor-nixdorf.cz>
2016-11-29 14:53:56 +0100

    OZK: RAM Plugin created.

---
+++ MonitoringAgent/PluginsCollection/RAM.plugin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualBasic.Devices;
+
+namespace PluginsCollection
+{
+    public class RAM : IPlugin
+    {
+        PluginOutputCollection _pluginOutputs;
+
+        public string Name
+        {
+            get
+            {
+                return "RAM usage";
+            }
+        }
+
+        public RAM()
+        {
+            _pluginOutputs = new PluginOutputCollection(Name);
+        }
+
+        public PluginOutputCollection Output()
+        {
+            string memoryUsage = string.Empty;
+            _pluginOutputs.PluginOutputList.Clear();
+
+            memoryUsage = GetTotalMemoryInBytes().ToString();
+           
+            return _pluginOutputs;
+        }
+        static ulong GetTotalMemoryInBytes()
+        {
+            return new Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory;
+        }
+    }
+}
