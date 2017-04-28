fcd20488aa84c60afba450de5b07b9c0f7a67350
Ondřej Zálešák <ondrej.zalesak@wincor-nixdorf.cz>
2017-04-28 15:08:16 +0200

    VS Verze, SQL plugin allowed

--- MonitoringAgent/PluginsCollection/SQLdbAvailabilityCheck.Plugin.cs
+++ MonitoringAgent/PluginsCollection/SQLdbAvailabilityCheck.Plugin.cs
@@ -1,63 +1,63 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Data.SqlClient;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using TPOMM.Tools;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPOMM.Tools;
 
-//namespace PluginsCollection
-//{
-//    public class SQLdbAvailabilityCheck : IPlugin
-//    {
-//        PluginOutputCollection _pluginOutputs;
-//        public string Name
-//        {
-//            get
-//            {
-//                return "SQLAvailability";
-//            }
-//        }
+namespace PluginsCollection
+{
+    public class SQLdbAvailabilityCheck : IPlugin
+    {
+        PluginOutputCollection _pluginOutputs;
+        public string Name
+        {
+            get
+            {
+                return "SQLAvailability";
+            }
+        }
 
-//        public string connString = TPOMM.Tools.Config.Current.cfConnectionString.ToString();
+        public string connString = TPOMM.Tools.Config.Current.cfConnectionString.ToString();
 
-//        public SQLdbAvailabilityCheck()
-//        {
-//            _pluginOutputs = new PluginOutputCollection(Name);
-//        }
+        public SQLdbAvailabilityCheck()
+        {
+            _pluginOutputs = new PluginOutputCollection(Name);
+        }
 
-//        public static bool IsServerConnected(string connectionString)
-//        {
-//            using (SqlConnection connection = new SqlConnection(TPOMM.Tools.Config.Current.ConnectionStrings["CFConnectionString"]))
-//            {
-//                try
-//                {
-//                    connection.Open();
-//                    return true;
-//                }
-//                catch (SqlException)
-//                {
-//                    return false;
-//                }
-//            }
-//        }
+        public static bool IsServerConnected(string connectionString)
+        {
+            using (SqlConnection connection = new SqlConnection(TPOMM.Tools.Config.Current.ConnectionStrings["CFConnectionString"]))
+            {
+                try
+                {
+                    connection.Open();
+                    return true;
+                }
+                catch (SqlException)
+                {
+                    return false;
+                }
+            }
+        }
 
-//        public PluginOutputCollection Output()
-//        {
-//            List<SimplePluginOutput> listSPO = new List<SimplePluginOutput>();
-//            _pluginOutputs.PluginOutputList.Clear();
+        public PluginOutputCollection Output()
+        {
+            List<SimplePluginOutput> listSPO = new List<SimplePluginOutput>();
+            _pluginOutputs.PluginOutputList.Clear();
 
-//            if (IsServerConnected(connString))
-//            {
-//                listSPO.Add(new SimplePluginOutput("Connnected", false));
-//            }
-//            else
-//            {
-//                listSPO.Add(new SimplePluginOutput("Disconnnected", true));
-//            }
-//            _pluginOutputs.PluginOutputList.Add(new PluginOutput("SQL Status", listSPO));
+            if (IsServerConnected(connString))
+            {
+                listSPO.Add(new SimplePluginOutput("Connnected", false));
+            }
+            else
+            {
+                listSPO.Add(new SimplePluginOutput("Disconnnected", true));
+            }
+            _pluginOutputs.PluginOutputList.Add(new PluginOutput("SQL Status", listSPO));
 
-//            return _pluginOutputs;
-//        }
-//    }
-//}
+            return _pluginOutputs;
+        }
+    }
+}
